8ab63a8197d5ef7aa55e19ea35dca403f2fe7d98
Cameron Eagans <me@cweagans.net>
2015-08-21 14:21:38 -0600

    Implement mcrypt_list_algorithms()

--- src/mcrypt.php
+++ src/mcrypt.php
@@ -165,6 +165,27 @@ if (!extension_loaded('mcrypt') || getenv('USE_RUNKIT')) {
      */
     function mcrypt_list_algorithms($lib_dir = null)
     {
+        return array (
+          0 => 'cast-128',
+          1 => 'gost',
+          2 => 'rijndael-128',
+          3 => 'twofish',
+          4 => 'arcfour',
+          5 => 'cast-256',
+          6 => 'loki97',
+          7 => 'rijndael-192',
+          8 => 'saferplus',
+          9 => 'wake',
+          10 => 'blowfish-compat',
+          11 => 'des',
+          12 => 'rijndael-256',
+          13 => 'serpent',
+          14 => 'xtea',
+          15 => 'blowfish',
+          16 => 'enigma',
+          17 => 'rc2',
+          18 => 'tripledes',
+        );
     }
 
     /**
